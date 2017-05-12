cfb093b85b8e3eb4ca2d2375519b1e2ddce34b72
Mikel Tuesta <mikel@lin3s.com>
2017-05-12 14:57:59 +0200

    Initial commit

---
+++ src/Resources/js/entry-styleguide.js
@@ -0,0 +1,16 @@
+/*
+ * This file is part of the Php DDD Standard project.
+ *
+ * Copyright (c) 2017 LIN3S <info@lin3s.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Beñat Espiña <bespina@lin3s.com>
+ */
+
+'use strict';
+
+import './../scss/styleguide/styleguide.scss';
+
+import './styleguide/styleguide';
---
+++ src/Resources/js/styleguide/styleguide.js
@@ -0,0 +1,15 @@
+/*
+ * This file is part of the Euskaltel project.
+ *
+ * Copyright (c) 2017 LIN3S <info@lin3s.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Beñat Espiña <bespina@lin3s.com>
+ */
+
+'use strict';
+
+import 'prismjs';
+import 'prismjs/components/prism-twig';
