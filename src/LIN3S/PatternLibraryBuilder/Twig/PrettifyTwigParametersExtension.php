5c0791a9bc4c72e0c20bac139a2fe17e299479c8
Mikel Tuesta <mikel@lin3s.com>
2017-06-02 12:33:55 +0200

    Added prettify_twig_parameters extension for prettiy-printing styleguide twig preview_parameters. Removed previous macro from the article.html.twig.

---
+++ src/LIN3S/PatternLibraryBuilder/Twig/PrettifyTwigParametersExtension.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+ * This file is part of the Pattern Library Builder project.
+ *
+ * Copyright (c) 2017-present LIN3S <info@lin3s.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace LIN3S\PatternLibraryBuilder\Twig;
+
+/**
+ * @author Mikel Tuesta <mikeltuesta@gmail.com>
+ */
+final class PrettifyTwigParametersExtension extends \Twig_Extension
+{
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('prettify_twig_parameters', [$this, 'prettifyTwigParameters']),
+        ];
+    }
+
+    public function prettifyTwigParameters($parameters)
+    {
+        return $this->prettifyParameters($parameters) . $this->newLine();
+    }
+
+    private function prettifyParameters($parameters, $str = '', $indentationLevel = 1)
+    {
+        if (is_array($parameters)) {
+            $parameterIndex = 0;
+            $parametersCount = count($parameters);
+
+            foreach ($parameters as $key => $value) {
+
+                if (!is_numeric($key)) {
+                    $str .= $this->newLine() . $this->indent($indentationLevel) . $key . ': ';
+                }
+
+                if (is_array($value)) {
+                    $isAssociative = $this->isAssociative($value);
+                    $str .= $isAssociative ? '{' : '[';
+                    $str = $this->prettifyParameters($value, $str, $isAssociative ? $indentationLevel + 1 : $indentationLevel);
+                    $str .= $isAssociative ? $this->newLine() . $this->indent($indentationLevel) . '}' : ']';
+                } else {
+                    $str .= '\''.$value.'\'';
+                }
+
+                $parameterIndex++;
+                if ($parameterIndex !== $parametersCount) {
+                    $str .= ',';
+                }
+            }
+        } else {
+            $str .= $parameters;
+        }
+
+        return $str;
+    }
+
+    private function isAssociative(array $arr)
+    {
+        if (array() === $arr) return false;
+        return array_keys($arr) !== range(0, count($arr) - 1);
+    }
+
+    private function newLine()
+    {
+        return "\n";
+    }
+
+    private function indent($indentationLevel)
+    {
+        $indentationStr = "";
+        $indentationIterations = 0;
+        while ($indentationIterations < $indentationLevel) {
+            $indentationStr .= "\t";
+            $indentationIterations++;
+        }
+        return $indentationStr;
+    }
+}
