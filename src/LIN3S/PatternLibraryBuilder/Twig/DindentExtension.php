5c490b033552330611711d34b59fa5aff5b6e83f
Mikel Tuesta <mikel@lin3s.com>
2017-06-05 11:33:32 +0200

    Updated html raw content output (tidy for dindent).

---
+++ src/LIN3S/PatternLibraryBuilder/Twig/DindentExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Pattern Library Builder project.
+ *
+ * Copyright (c) 2017-present LIN3S <info@lin3s.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace LIN3S\PatternLibraryBuilder\Twig;
+
+use Gajus\Dindent\Indenter;
+
+/**
+ * @author Beñat Espiña <benatespina@gmail.com>
+ */
+class DindentExtension extends \Twig_Extension
+{
+    public function getFilters(): array
+    {
+        return [
+            new \Twig_SimpleFilter('dindent', [$this, 'dindent']),
+        ];
+    }
+
+    public function dindent(string $html): string
+    {
+        $indenter = new Indenter();
+
+        return htmlspecialchars($indenter->indent($html));
+    }
+}
